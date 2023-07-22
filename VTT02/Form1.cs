975cfe9b8523537591fe9ca489afcd4fccfeb410
id <ismail.demir@emikon.com.tr>
2023-07-22 12:11:48 +0200

    First

---
+++ VTT02/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using VTT02.MySockets;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace VTT02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        MyListener TCPListen                               = new MyListener(4545);
+         
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TCPListen.StartListen();
+            TCPListen.ReceivedOk += new MyListener.DlgRecivedOk(TCPOkumaOk);       
+        }
+
+        void TCPOkumaOk(string ReceiveString, string IPAdress)
+        {
+            textBox1.Text                                   = ReceiveString;
+            textBox2.Text                                   = IPAdress;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Socket server;// Declare your Socket
+            //IPEndPoint ip = new IPEndPoint(IPAddress.Parse("10.126.225.44"), 4545);
+            IPEndPoint ip; // Declare your IPEndpoint
+
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ip = new IPEndPoint(IPAddress.Parse("192.168.0.46"), 4545);
+            server.Connect(ip);
+
+            if (server.Connected == true)
+            {
+
+                String szData = "00200001000000000000#";//Nutrunner Communication Start
+                byte[] byData = Encoding.Default.GetBytes(szData);//System.Text.Encoding.ASCII.GetBytes(szData);
+
+                int sent = server.Send(byData, SocketFlags.None);
+            }
+        }
+    }
+}
---
+++ VTT02/MySockets/MyListener.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace VTT02.MySockets
+{
+    class MyListener
+    {
+#region Global Değişkenler
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        Socket               SocketListener, SocketClient;
+        int                  Port;
+        byte[]               Buffer = new byte[1024];
+
+        public delegate void DlgRecivedOk(string ReceivedString, string IPAdress);
+        public DlgRecivedOk  ReceivedOk;
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+
+#region MyListener Class'ını kur
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        public MyListener(int PortNo)
+        {
+            this.Port                                       = PortNo;
+            SocketListener                                  = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+
+#region Start Listen - Dinlemeyi Başlat
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        public void StartListen()
+        {
+            SocketListener.Bind(new IPEndPoint(IPAddress.Any, this.Port));
+            
+// Kaç adet bağlantıyı kabul edecek ?
+            SocketListener.Listen(100);
+
+// Bağlantıyı kabul etmeyi başlat
+            SocketListener.BeginAccept(new AsyncCallback(OnAccept), null);
+        }
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+
+#region OnAccept - Bağlantı kabul edildi
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        private void OnAccept(IAsyncResult ar)
+        {
+// Bağlantıyı sonlandır & Soketi ClientSocket'e aktar.
+            SocketClient                                    = SocketListener.EndAccept(ar);
+
+// Start Data Receive
+            StartDataReceive();
+        }
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+
+#region Start Data Receive - Byte Array almayı başlat
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        private void StartDataReceive()
+        {
+            SocketClient.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+        }
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+
+#region OnReceive - Byte Array alındı
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+        private void OnReceive(IAsyncResult ar)
+        {
+// Almayı sonlandır. & Gelen byte'ın boyutu
+            int Length                                      = SocketClient.EndReceive(ar);
+
+// Bağlantı koptu
+            if (Length <= 0)                                                                   
+                return;
+
+// Gelen byte'ı işle & String'e çevir.
+            byte[] SizedBuffer                              = new byte[Length];
+            Array.Copy(Buffer, SizedBuffer, Length);
+
+// ReceivedOk Evet'ını tetikle & Strin'i dışarıya ver
+            ReceivedOk(Encoding.ASCII.GetString(SizedBuffer), IPAddress.Parse(((IPEndPoint)SocketClient.RemoteEndPoint).Address.ToString()).ToString());
+
+// Data Receive tekrar başlat
+            SocketClient.BeginReceive(Buffer, 0, Buffer.Length, SocketFlags.None, new AsyncCallback(OnReceive), null);
+        }
+// --------------------------------------------------------------------------------------------------------------------------------------- 
+#endregion
+    }
+}
